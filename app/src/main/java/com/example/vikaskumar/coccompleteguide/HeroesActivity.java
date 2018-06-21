79aa5cf7f0e37c5ecdd00397f14a6115ecf21d84
Ankur Kashyap <shiv.ankurkashyap@gmail.com>
2018-06-22 02:08:49 +0530

    Added curved header

---
+++ app/src/main/java/com/example/vikaskumar/coccompleteguide/HeroesActivity.java
@@ -0,0 +1,13 @@
+package com.example.vikaskumar.coccompleteguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeroesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heroes);
+    }
+}
